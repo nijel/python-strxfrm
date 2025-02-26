2426c4d656170d3b195a3ea9905c37bb60af5cfc
Michal Čihař <michal@cihar.com>
2025-02-26 10:57:28 +0100

    remove en locale

--- test.py
+++ test.py
@@ -5,13 +5,11 @@ import locale
 import platform
 
 WORDS = ('zkouška', 'zkouzka', 'zkouaka', 'Français', '中文')
-LOCALES = (None, "C", "en", ("en_US", "UTF-8"), ("C", "UTF-8"))
+LOCALES = (None, "C", ("en_US", "UTF-8"), ("C", "UTF-8"))
 
 FIXTURE = [(locale_name, word) for locale_name in LOCALES for word in WORDS]
 
 @pytest.mark.parametrize("locale_name,word", FIXTURE)
 def test(locale_name, word):
-    if locale_name == "en" and platform.system() == "Linux":
-         pytest.skip("non-UTF-8 locales are no longer supported on Linux")
     locale.setlocale(locale.LC_ALL, locale_name)
     print(f"strxfrm({word!r}) = {locale.strxfrm(word)!r}")
