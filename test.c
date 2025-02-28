f9b997b4308c0cda3df84fd2fd2ff08aed3e2e29
Michal Čihař <michal@cihar.com>
2025-02-28 13:51:20 +0100

    add c test

---
+++ test.c
@@ -0,0 +1,19 @@
+#include <locale.h>
+#include <string.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <wchar.h>
+
+int main(int argc, char* argv) {
+    wchar_t *buf;
+    size_t len;
+    wchar_t string[] = L"š";
+
+    buf = malloc(sizeof(wchar_t) * 40);
+
+    printf("setlocale=%s\n", setlocale(LC_ALL, "en_US.UTF-8"));
+
+    printf("wcsxfrm(1)=%zd\n", wcsxfrm(buf, string, 1));
+    printf("wcsxfrm(40)=%zd\n", wcsxfrm(buf, string, 40));
+}
