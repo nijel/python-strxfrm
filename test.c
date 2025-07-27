6e0cf4a4699d6c0c9b93e7601d002db06d01927a
Michal Čihař <michal@cihar.com>
2025-07-27 08:26:55 +0200

    repeated execution in C

--- test.c
+++ test.c
@@ -11,16 +11,19 @@ int main(int argc, char** argv) {
     size_t len;
     wchar_t string[] = L"zkouška žřščřščřšě";
     wchar_t string2[] = L"zkouska zrscrscrse";
+    int i;
 
     buf = malloc(sizeof(wchar_t) * 400);
 
     printf("setlocale=%s\n", setlocale(LC_ALL, "en_US.UTF-8"));
 
-    errno = 0;
-    printf("wcsxfrm(1)=%zd / %d\n", wcsxfrm(buf, string, 1), errno);
-    errno = 0;
-    printf("wcsxfrm(40)=%zd / %d \n", wcsxfrm(buf, string, 400), errno);
-    printf("buf=%ls\n", buf);
-    printf("wcsxfrm(40)=%zd / %d \n", wcsxfrm(buf, string2, 400), errno);
-    printf("buf=%ls\n", buf);
+    for (i = 0; i < 10000; i++ ) {
+        errno = 0;
+        printf("wcsxfrm(1)=%zd / %d\n", wcsxfrm(buf, string, 1), errno);
+        errno = 0;
+        printf("wcsxfrm(40)=%zd / %d \n", wcsxfrm(buf, string, 400), errno);
+        printf("buf=%ls\n", buf);
+        printf("wcsxfrm(40)=%zd / %d \n", wcsxfrm(buf, string2, 400), errno);
+        printf("buf=%ls\n", buf);
+    }
 }
