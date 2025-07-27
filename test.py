11917d09a41fb1ecc1f71fa63a64f633c1d01912
Michal Čihař <michal@cihar.com>
2025-07-27 08:16:01 +0200

    test languages sorting

--- test.py
+++ test.py
@@ -3,6 +3,7 @@
 import pytest
 import locale
 import platform
+from weblate_language_data.languages import LANGUAGES
 
 WORDS = ('zkouška', 'zkouzka', 'zkouaka', 'Français', '中文', 'š')
 LOCALES = (None, "en_US.UTF-8")
@@ -13,3 +14,7 @@ FIXTURE = [(locale_name, word) for locale_name in LOCALES for word in WORDS]
 def test(locale_name, word):
     locale.setlocale(locale.LC_ALL, locale_name)
     print(f"strxfrm({word!r}) = {locale.strxfrm(word)!r}")
+
+def test_languages():
+    locale.setlocale(locale.LC_ALL, ("en_US", "UTF-8"))
+    sorted(LANGUAGES, key=lambda x: locale.strxfrm(x[1]))
